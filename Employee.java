55661aef9a8c77d1a0455951ff9a452131091051
Iramtegbrahana123 <sayednsrddn@gmail.com>
2025-06-02 20:04:39 +0530

    local

---
+++ Employee.java
@@ -0,0 +1,22 @@
+public class Employee extends Person {
+    String empId;
+    int salary;
+
+    public Employee(String name, String dob, String empId, int salary) {
+        super(name, dob); // Call parent constructor
+        this.empId = empId;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        displayName();
+        displayAge();
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee e = new Employee("iram", "2005-05-08", "EMP101", 40000);
+        e.displayDetails();
+    }
+}
